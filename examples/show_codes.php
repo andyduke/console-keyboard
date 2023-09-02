0a6faec9e0cf3d638601d92127de7775b210aafe
Andy Chentsov <chentsov@gmail.com>
2023-09-03 00:27:07 +0300

    Simplify the example

--- examples/show_codes.php
+++ examples/show_codes.php
@@ -1,19 +1,12 @@
 <?php
 
 use AndyDuke\ConsoleKeyboard\Keyboard;
-use AndyDuke\ConsoleKeyboard\Win32Keyboard;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$k = null;
-
 echo "Press any key to display key code or press Q to exit:\n\n";
 
-$k = Keyboard::create([
-  'win32' => [
-    //'eventType' => Win32Keyboard::EVENT_KEYUP,
-  ],
-]);
+$k = Keyboard::create();
 
 foreach($k->readKey() as $key) {
   // Ctrl-C or Error
