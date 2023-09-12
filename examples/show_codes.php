1825153f28f6d008868a1de6327ac1c1389d1d66
Andy Chentsov <chentsov@gmail.com>
2023-09-12 16:17:52 +0300

    Update an example for compatibility with Linux

--- examples/show_codes.php
+++ examples/show_codes.php
@@ -4,6 +4,12 @@ use AndyDuke\ConsoleKeyboard\Keyboard;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+function convertRawCode($code) {
+  return (PHP_OS == 'WINNT')
+    ? $code
+    : str_replace("\033[", '<ESC>', $code);
+}
+
 echo "Press any key to display key code or press Q to exit:\n\n";
 
 $k = Keyboard::create();
@@ -18,7 +24,7 @@ foreach($k->readKey() as $key) {
     break;
   }
 
-  echo $key->getKey() . ' (' . $key->getRawKey() . ')' . PHP_EOL;
+  echo $key->getKey() . ' (' . convertRawCode($key->getRawKey()) . ')' . PHP_EOL;
 }
 
 echo "\nExit.\n";
