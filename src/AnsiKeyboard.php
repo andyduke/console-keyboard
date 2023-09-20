f6b9b4438ee39854c006298ab347fcdeb1e040b8
Andy Chentsov <chentsov@gmail.com>
2023-09-20 13:11:39 +0300

    Better keys mapping (without using json_decode) #14

--- src/AnsiKeyboard.php
+++ src/AnsiKeyboard.php
@@ -10,72 +10,64 @@ class AnsiKeyboard extends Keyboard {
   private const CTRL_C = 'ctrl-c';
 
   /**
-   * Plain text char map
+   * Char map
    */
-  private $textMap = [
+  private $keyMap = [
+    // plain text sequences
     "\n"  => self::ENTER,
     ' '   => self::SPACE,
     "\t"  => self::TAB,
-  ];
 
-  /**
-   * Ascii char map
-   */
-  private $asciiMap = [
-    3     => self::CTRL_C,
-    127   => self::BACKSPACE, // \u0008 doesn't work
-  ];
+    // special keys
+    "\003" => self::CTRL_C,
+    "\x7F" => self::BACKSPACE,
 
-  /**
-   * Unicode char map
-   */
-  private $unicodeMap = [
     // main keys
-    '\u001b' => self::ESC,
+    "\u{001b}" => self::ESC,
 
     // arrow keys
-    '\u001b[A' => self::UP,
-    '\u001b[B' => self::DOWN,
-    '\u001b[D' => self::LEFT,
-    '\u001b[C' => self::RIGHT,
+    "\u{001b}[A" => self::UP,
+    "\u{001b}[B" => self::DOWN,
+    "\u{001b}[D" => self::LEFT,
+    "\u{001b}[C" => self::RIGHT,
 
     // function keys
-    '\u001b[1P'  => self::F1,
-    '\u001b[1Q'  => self::F2,
-    '\u001b[1R'  => self::F3,
-    '\u001b[1S'  => self::F4,
-    '\u001b[11~' => self::F1,
-    '\u001b[12~' => self::F2,
-    '\u001b[13~' => self::F3,
-    '\u001b[14~' => self::F4,
-    '\u001b[15~' => self::F5,
-    '\u001b[17~' => self::F6,
-    '\u001b[18~' => self::F7,
-    '\u001b[19~' => self::F8,
-    '\u001b[20~' => self::F9,
-    '\u001b[21~' => self::F10,
-    '\u001b[23~' => self::F11,
-    '\u001b[24~' => self::F12,
-    '\u001b[25~' => self::F13,
-    '\u001b[26~' => self::F14,
-    '\u001b[28~' => self::F15,
-    '\u001b[29~' => self::F16,
-    '\u001b[31~' => self::F17,
-    '\u001b[32~' => self::F18,
-    '\u001b[33~' => self::F19,
-    '\u001b[34~' => self::F20,
+    "\u{001b}[1P"  => self::F1,
+    "\u{001b}[1Q"  => self::F2,
+    "\u{001b}[1R"  => self::F3,
+    "\u{001b}[1S"  => self::F4,
+    "\u{001b}[11~" => self::F1,
+    "\u{001b}[12~" => self::F2,
+    "\u{001b}[13~" => self::F3,
+    "\u{001b}[14~" => self::F4,
+    "\u{001b}[15~" => self::F5,
+    "\u{001b}[17~" => self::F6,
+    "\u{001b}[18~" => self::F7,
+    "\u{001b}[19~" => self::F8,
+    "\u{001b}[20~" => self::F9,
+    "\u{001b}[21~" => self::F10,
+    "\u{001b}[23~" => self::F11,
+    "\u{001b}[24~" => self::F12,
+    "\u{001b}[25~" => self::F13,
+    "\u{001b}[26~" => self::F14,
+    "\u{001b}[28~" => self::F15,
+    "\u{001b}[29~" => self::F16,
+    "\u{001b}[31~" => self::F17,
+    "\u{001b}[32~" => self::F18,
+    "\u{001b}[33~" => self::F19,
+    "\u{001b}[34~" => self::F20,
 
     // other keys
-    '\u001b[2~' => self::INS,
-    '\u001b[3~' => self::DEL,
-    '\u001b[1~' => self::HOME,
-    '\u001b[7~' => self::HOME,
-    '\u001b[H'  => self::HOME,
-    '\u001b[4~' => self::END,
-    '\u001b[8~' => self::END,
-    '\u001b[F'  => self::END,
-    '\u001b[5~' => self::PGUP,
-    '\u001b[6~' => self::PGDOWN,
+    "\u{001b}[2~" => self::INS,
+    "\u{001b}[3~" => self::DEL,
+    "\u{001b}[1~" => self::HOME,
+    "\u{001b}[7~" => self::HOME,
+    "\u{001b}[H"  => self::HOME,
+    "\u{001b}[4~" => self::END,
+    "\u{001b}[8~" => self::END,
+    "\u{001b}[F"  => self::END,
+    "\u{001b}[5~" => self::PGUP,
+    "\u{001b}[6~" => self::PGDOWN,
   ];
 
   private $queue;
@@ -158,32 +150,12 @@ class AnsiKeyboard extends Keyboard {
   private function translateInput(?string $input): ?string {
     if ($input == null) return null;
 
-    // try matching the key with plain text
-    foreach($this->textMap as $code => $key) {
-      if ($code === $input) {
-        return $key;
-      }
-    }
-
-    // try matching the key with ascii
-    foreach($this->asciiMap as $code => $key) {
-      if (chr($code) === $input) {
-        return $key;
-      }
-    }
-
-    // try matching the key with unicode
-    foreach($this->unicodeMap as $code => $key) {
-      if ($this->unicodeToString($code) === $input) {
-        return $key;
-      }
+    // try matching the key with table
+    if (isset($this->keyMap[$input])) {
+      return $this->keyMap[$input];
     }
 
     return null;
-  }
-
-  private function unicodeToString($code) {
-    return json_decode('"' . $code . '"');
   }
 
 }
